817e69b7694d9267af439be7290bcc19bce03d4b
Kiaizen <kiaizenx@gmail.com>
2022-11-14 22:16:31 -0300

    Inserindo valores na tabela objetos

--- Atividade01.sql
+++ Atividade01.sql
@@ -5,3 +5,13 @@ create table objetos(
     tipo_objeto varchar(60) not null,
     funciona boolean
 );
+
+insert into objetos(nome , tipo_objeto, funciona)
+VALUES('Lousa', 'Madeira', true);
+
+insert into objetos(nome , tipo_objeto, funciona)
+VALUES('Caneta', 'Plástico', false);
+
+insert into objetos(nome , tipo_objeto, funciona)
+VALUES('Caderno', 'Madeira', true);
+
